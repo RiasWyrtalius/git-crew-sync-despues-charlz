86876f34f2a4442c148ccedbb0f0c638c2ddae0b
Rias <charlzdespues.cd@gmail.com>
2026-09-21 20:46:10 +0800

    Update calculatePay return formatting - despues.charlz

--- shifts.js
+++ shifts.js
@@ -4,11 +4,11 @@ function isValidShift(hours) {
 
 function calculatePay(hours, rate) {
   if (hours <= 8) {
-    return Math.round(hours * rate);
+    return Math.floor(hours * rate);
   }
   const regularPay = 8 * rate;
   const overtimePay = (hours - 8) * rate * 1.5;
-  return Math.round(regularPay + overtimePay);
+  return Number(Math.floor(regularPay + overtimePay));
 }
 
 module.exports = { isValidShift, calculatePay };
