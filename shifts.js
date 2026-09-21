115d301dc907013063b7aba848e04401a04a8922
Rias <charlzdespues.cd@gmail.com>
2026-09-21 21:59:46 +0800

    Fix normal shift pay to truncate using Math.floor - despues.charlz

--- shifts.js
+++ shifts.js
@@ -4,7 +4,7 @@ function isValidShift(hours) {
 
 function calculatePay(hours, rate) {
   if (hours <= 8) {
-    return Math.round(hours * rate);
+    return Math.floor(hours * rate);
   }
   const regularPay = 8 * rate;
   const overtimePay = (hours - 8) * rate * 1.5;
